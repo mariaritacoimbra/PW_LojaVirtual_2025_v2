ed27accf9f89d54859c0cdc2d0a04a85f20731d0
Maria Rita Coimbra <rosacoimbramariarita@gmail.com>
2025-05-28 16:21:49 -0300

    Refactor database operations to use context managers and return appropriate values; add product detail page template.

--- data/cliente_repo.py
+++ data/cliente_repo.py
@@ -1,39 +1,38 @@
+from typing import Optional
 from data.cliente_model import Cliente
 from data.cliente_sql import *
 from data.util import get_connection
 
-def criar_tabela():
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute(CRIAR_TABELA)
-    conn.commit()
-    conn.close()
+def criar_tabela() -> bool:
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(CRIAR_TABELA)
+        return cursor.rowcount > 0
 
-def inserir(cliente: Cliente):
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute(INSERIR, (
-        cliente.nome, 
-        cliente.cpf, 
-        cliente.email, 
-        cliente.telefone, 
-        cliente.senha))
-    conn.commit()
-    conn.close()
 
-def obter_todos() -> list[Cliente]:
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute(OBTER_TODOS)
-    tuplas = cursor.fetchall()
-    conn.close()
-    clientes = [
-        Cliente(
-            id=tupla[0], 
-            nome=tupla[1], 
-            cpf=tupla[2],
-            email=tupla[3],
-            telefone=tupla[4],
-            senha=tupla[5]) 
-            for tupla in tuplas]
-    return clientes
+def inserir(cliente: Cliente) -> Optional[int]:
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(INSERIR, (
+            cliente.nome, 
+            cliente.cpf, 
+            cliente.email, 
+            cliente.telefone, 
+            cliente.senha))
+        return cursor.lastrowid
+        
+def obter_todos() -> list[Cliente] :
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(OBTER_TODOS)
+        rows = cursor.fetchall()
+        clientes = [
+            Cliente(
+                id=row[0], 
+                nome=row[1], 
+                cpf=row[2],
+                email=row[3],
+                telefone=row[4],
+                senha=row[5]) 
+                for row in rows]
+        return clientes
